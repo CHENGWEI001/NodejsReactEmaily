925eb51169a52edddf35019ff8f72a0cd4c3045f
ch001 <rg550black@gmail.com>
2018-11-26 22:11:20 +0000

    2nd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({'hi': 'there'});
+    res.send({'hi': 'there2'});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
