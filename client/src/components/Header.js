1d559bdb5fe8958a7d3cc832b1e7152a8588e394
EC2 Default User <ec2-user@ip-172-31-20-220.us-west-2.compute.internal>
2018-12-06 01:54:39 +0000

    after 83

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login With Google</a></li>
+                );
+            default:
+                return (
+                    <li><a href="/api/logout">Logout</a></li>
+                );
+        }
+    }
+    
     render() {
         return (
           <nav>
             <div className="nav-wrapper">
               <a href="#" className="left brand-logo">Emaily</a>
               <ul className="right">
-                <li><a href="#">Login With Google</a></li>
+                {this.renderContent()}
               </ul>
             </div>
           </nav>
@@ -15,4 +31,12 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+// function mapStateToProps(state) {
+//   return { auth: state.auth };
+// }
+
+export default connect(mapStateToProps)(Header);
