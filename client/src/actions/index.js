b5e163e0634c14215362f20ddf96e1d03683dd3f
EC2 Default User <ec2-user@ip-172-31-20-220.us-west-2.compute.internal>
2018-12-06 01:16:34 +0000

    after 81

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './types';
 // https://www.udemy.com/node-with-react-fullstack-web-development/learn/v4/t/lecture/7605096?start=440
 export const fetchUser = () => async dispatch => {
         const res = await axios.get('/api/current_user')
-        dispatch({type: FETCH_USER,payload: res});
+        dispatch({type: FETCH_USER,payload: res.data});
 }
 
 // without redux-thunk
