2e50c9cec028d6d95e88fa21ab628aa69474225d
EC2 Default User <ec2-user@ip-172-31-20-220.us-west-2.compute.internal>
2018-12-10 23:43:20 +0000

    with client and push to Heroku

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -11,9 +13,13 @@ class Header extends Component {
                     <li><a href="/auth/google">Login With Google</a></li>
                 );
             default:
-                return (
-                    <li><a href="/api/logout">Logout</a></li>
-                );
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="3" style={{margin: '0 10px'}}>
+                      Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
     
@@ -21,7 +27,12 @@ class Header extends Component {
         return (
           <nav>
             <div className="nav-wrapper">
-              <a href="#" className="left brand-logo">Emaily</a>
+              <Link 
+                to={this.props.auth ? '/surveys': '/'} 
+                className="left brand-logo"
+              >
+                Emaily
+                </Link>
               <ul className="right">
                 {this.renderContent()}
               </ul>
@@ -34,6 +45,8 @@ class Header extends Component {
 function mapStateToProps({ auth }) {
   return { auth };
 }
+
+
 
 // function mapStateToProps(state) {
 //   return { auth: state.auth };
--- index.js
+++ index.js
@@ -5,12 +5,14 @@ require("./services/passport"); // because the purpose here is just to execute t
 const mongoose  = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require('body-parser');
 const keys = require("./config/keys");
 const port = process.env.PORT || 5000;
 
 // https://community.c9.io/t/setting-up-mongodb/1717
 mongoose.connect(keys.mongoURI);
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,11 +24,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 app.get('/', (req, res) => {
     res.send({'hi': 'there2'});
 });
 
-
+if (process.env.NODE_ENV === 'production') {
+     app.use(express.static('client/build'));
+     
+     const path = require('path');
+     app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+     });
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
